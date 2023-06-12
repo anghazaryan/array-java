898a423e7f73d41431ed087f726915d1c296b3e8
anghazaryan <anahit.ghazaryan91@gmail.com>
2023-06-12 19:07:33 +0400

    first commit

---
+++ Array.java
@@ -0,0 +1,33 @@
+public class Array {
+	
+	public static void main (String[] args){
+
+
+        byte[] byteNumbers = new byte[7];
+        short[] shortNumbers = new short[8];
+        int[] intNumbers = new int[5];
+        long[] longNumbers = new long[9];
+        float[] floatNumbers = new float[15];
+        double[] doubleNumbers = new double[17];
+
+        String [] stringNames = new String[6];    
+        stringNames[0]="Anahit";
+        stringNames[1]="Tatev";
+        stringNames[2]="Irina";
+        stringNames[3]="Gagik";
+        stringNames[4]="Elen";
+        stringNames[5]="Anna";
+
+        int[] peopleAges = new int[6];
+        peopleAges[0] = 31;
+        peopleAges[1] = 32;
+        peopleAges[2] = 33;
+        peopleAges[3] = 34;
+        peopleAges[4] = 35;
+        peopleAges[5] = 36;
+
+        System.out.println("Name is " + stringNames[4] + " " + "and age is " + peopleAges[4]);
+
+    		  }
+    	   }
+
